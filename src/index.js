bc0712a8bed2fd681e67c7711e6cf30e1fc0b35c
killeraliens <killeraliens@outlook.com>
2019-08-07 20:46:55 -0700

    removed playground

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
-// import STORE from './store.js';
-import HelloWorld from './state/HelloWorld';
+import App from './App';
+import STORE from './store.js';
 
-// console.log(STORE);
-// ReactDOM.render(<App store={STORE} />, document.getElementById('root'));
- ReactDOM.render(<HelloWorld />, document.getElementById('root'));
+
+console.log(STORE);
+ReactDOM.render(<App store={STORE} />, document.getElementById('root'));
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
